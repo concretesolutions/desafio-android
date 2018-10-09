27044c7d0ca0e6b14bb7b679c2a0ed7b0a2e2773
Francisco Moya <f.moya.gonzalez@accenture.com>
2018-10-09 16:49:21 -0300

    Agregando pruebas con expresso

--- app/src/androidTest/java/com/mbstro/fmoyagonzalez/desafio_android/MainActivityTest.kt
+++ app/src/androidTest/java/com/mbstro/fmoyagonzalez/desafio_android/MainActivityTest.kt
@@ -5,10 +5,10 @@ import android.support.test.espresso.Espresso.onView
 import android.support.test.espresso.Espresso.pressBack
 import android.support.test.espresso.action.ViewActions
 import android.support.test.espresso.action.ViewActions.click
+import android.support.test.espresso.assertion.ViewAssertions.matches
+import android.support.test.espresso.contrib.RecyclerViewActions
 import android.support.test.espresso.contrib.RecyclerViewActions.actionOnItemAtPosition
-import android.support.test.espresso.matcher.ViewMatchers.withClassName
-import android.support.test.espresso.matcher.ViewMatchers.withClassName
-import android.support.test.espresso.matcher.ViewMatchers.withId
+import android.support.test.espresso.matcher.ViewMatchers.*
 import android.support.test.filters.LargeTest
 import android.support.test.rule.ActivityTestRule
 import android.support.test.runner.AndroidJUnit4
@@ -24,6 +24,8 @@ import org.hamcrest.TypeSafeMatcher
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
+import android.support.v7.widget.RecyclerView
+import android.support.test.espresso.matcher.BoundedMatcher
 
 
 @LargeTest
@@ -35,16 +37,43 @@ class MainActivityTest {
     var mActivityTestRule = ActivityTestRule(MainActivity::class.java)
 
     @Test
-    fun mainActivityTest() {
+    fun swipeAndClickFirstRepo() {
+        Thread.sleep(6000)
         val recyclerView = onView(
                 allOf(withId(R.id.recycler_view),
                         childAtPosition(
                                 withClassName(`is`("android.support.constraint.ConstraintLayout")),
                                 0)))
-        Thread.sleep(6000)
+        onView(withId(R.id.recycler_view))
+                .check(matches(atPosition(0, withText("java-design-patterns"))))
         recyclerView.perform(actionOnItemAtPosition<ViewHolder>(0, click()))
         Thread.sleep(2000)
         pressBack()
+
+        recyclerView.perform(ViewActions.swipeUp())
+        Thread.sleep(1000)
+        recyclerView.perform(ViewActions.swipeUp())
+        Thread.sleep(1000)
+        recyclerView.perform(ViewActions.swipeUp())
+        Thread.sleep(1000)
+        recyclerView.perform(ViewActions.swipeUp())
+        Thread.sleep(1000)
+        recyclerView.perform(ViewActions.swipeUp())
+        Thread.sleep(6000)
+        recyclerView.perform(ViewActions.swipeUp())
+
+    }
+
+    @Test
+    fun clickPullRequest(){
+        Thread.sleep(6000)
+        val recyclerView = onView(
+                allOf(withId(R.id.recycler_view),
+                        childAtPosition(
+                                withClassName(`is`("android.support.constraint.ConstraintLayout")),
+                                0)))
+        recyclerView.perform(actionOnItemAtPosition<ViewHolder>(0, click()))
+        Thread.sleep(2000)
 
         val recyclerView2 = onView(
                 allOf(withId(R.id.recycler_view_pull_request),
@@ -52,19 +81,7 @@ class MainActivityTest {
                                 withClassName(`is`("android.support.constraint.ConstraintLayout")),
                                 2)))
 
-        //recyclerView2.perform(actionOnItemAtPosition<ViewHolder>(0, click()))
-
-        recyclerView.perform(ViewActions.swipeUp())
-        Thread.sleep(1000)
-        recyclerView.perform(ViewActions.swipeUp())
-        Thread.sleep(1000)
-        recyclerView.perform(ViewActions.swipeUp())
-        Thread.sleep(1000)
-        recyclerView.perform(ViewActions.swipeUp())
-        Thread.sleep(1000)
-        recyclerView.perform(ViewActions.swipeUp())
-        Thread.sleep(6000)
-        recyclerView.perform(ViewActions.swipeUp())
+        recyclerView2.perform(actionOnItemAtPosition<ViewHolder>(0, click()))
 
     }
 
@@ -86,19 +103,38 @@ class MainActivityTest {
         }
     }
 
-    fun isInTheMiddle(text : String): Matcher<RepoAdapter.ViewHolder> {
-        return object : TypeSafeMatcher<RepoAdapter.ViewHolder>() {
-            override fun matchesSafely(item: RepoAdapter.ViewHolder ): Boolean {
-                val nameRepo = item.itemView.findViewById(R.id.name_repo) as TextView
-                        ?: return false
-                return nameRepo.text.toString().contains(text)
+    private fun atPosition(position: Int, itemMatcher: Matcher<View>): Matcher<View> {
+        checkNotNull(itemMatcher)
+        return object : BoundedMatcher<View, RecyclerView>(RecyclerView::class.java) {
+            override fun describeTo(description: Description) {
+                description.appendText("has item at position $position: ")
+                itemMatcher.describeTo(description)
             }
 
-            override fun describeTo(description: Description) {
-                description.appendText("first item with repo name ")
+            override fun matchesSafely(view: RecyclerView): Boolean {
+                val va = view.adapter?.itemCount
+
+                val viewHolder = view.findViewHolderForAdapterPosition(position)
+                        ?: // has no item on such position
+                        return false
+                val sr: TextView = viewHolder.itemView.findViewById(R.id.name_repo);
+                return itemMatcher.matches(sr)
             }
         }
     }
 
+    private fun count(itemMatcher: Matcher<Integer>): BoundedMatcher<View, RecyclerView> {
+        checkNotNull(itemMatcher)
+        return object : BoundedMatcher<View, RecyclerView>(RecyclerView::class.java) {
+            override fun describeTo(description: Description) {
+                description.appendText("has item at position : ")
+                itemMatcher.describeTo(description)
+            }
 
+            override fun matchesSafely(view: RecyclerView): Boolean {
+                val va = view.adapter?.itemCount
+                return itemMatcher.matches(va)
+            }
+        }
+    }
 }
