6cc2751a101207e7845f4fad60d29e7f4dfd332c
Rafael R <raha148@hotmail.com>
2018-10-14 12:25:41 -0300

    Criação do layout de repositórios

--- app/src/main/java/com/rafaelpereiraramos/desafioAndroid/view/RepoPagedListaAdapter.kt
+++ app/src/main/java/com/rafaelpereiraramos/desafioAndroid/view/RepoPagedListaAdapter.kt
@@ -1,7 +1,80 @@
 package com.rafaelpereiraramos.desafioAndroid.view
 
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.paging.PagedListAdapter
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.rafaelpereiraramos.desafioAndroid.R
+import com.rafaelpereiraramos.desafioAndroid.database.model.RepoTO
+
 /**
  * Created by Rafael P. Ramos on 13/10/2018.
  */
-class RepoPagedListaAdapter {
+class RepoPagedListaAdapter : PagedListAdapter<RepoTO, RepoPagedListaAdapter.RepoViewHolder>(REPO_COMPARATOR) {
+
+    companion object {
+        private val REPO_COMPARATOR = object : DiffUtil.ItemCallback<RepoTO>() {
+            override fun areItemsTheSame(oldItem: RepoTO, newItem: RepoTO): Boolean =
+                    oldItem.fullName == newItem.fullName
+
+            override fun areContentsTheSame(oldItem: RepoTO, newItem: RepoTO): Boolean =
+                    oldItem == newItem
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RepoViewHolder = RepoViewHolder.create(parent)
+
+    override fun onBindViewHolder(holder: RepoViewHolder, position: Int) {
+        val repoItem = getItem(position)
+        holder.bind(repoItem)
+    }
+
+    class RepoViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+
+        companion object {
+            fun create(parent: ViewGroup): RepoViewHolder {
+                val view = LayoutInflater.from(parent.context)
+                        .inflate(R.layout.row_repo_list, parent, false)
+                return RepoViewHolder(view)
+            }
+        }
+
+        private val _repoTitle: TextView = view.findViewById(R.id.repo_title)
+        private val _repoDescription: TextView = view.findViewById(R.id.repo_description)
+        private val _starts: Button = view.findViewById(R.id.stargazers)
+        private val _forks: Button = view.findViewById(R.id.forks)
+        private val _repoImg: ImageView = view.findViewById(R.id.repo_img)
+        private val _ownerName: TextView = view.findViewById(R.id.owner_name)
+
+        private var repo: RepoTO? = null
+
+        fun bind(repo: RepoTO?) {
+            if (repo == null) {
+                /*val resources = itemView.resources
+                name.text = resources.getString(R.string.loading)
+                description.visibility = View.GONE
+                language.visibility = View.GONE
+                stars.text = resources.getString(R.string.unknown)
+                forks.text = resources.getString(R.string.unknown)*/
+            } else {
+                showRepoData(repo)
+            }
+        }
+
+        private fun showRepoData(repo: RepoTO) {
+            this.repo = repo
+            _repoTitle.text = repo.fullName
+            _repoDescription.text = repo.description
+
+            _starts.text = repo.stargazers.toString()
+            _forks.text = repo.forks.toString()
+
+            _ownerName.text = repo.owner.login
+        }
+    }
 }
