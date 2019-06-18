fad03385beb7690d51a9f2e5221f0c9a9203b6ec
Fernando Gomes <lfgsnando@gmail.com>
2019-06-18 09:45:48 -0300

    add getters and setters to repo and user class

--- github_javapop/app/src/main/java/com/lfernando/githubjavapop/model/Repo.java
+++ github_javapop/app/src/main/java/com/lfernando/githubjavapop/model/Repo.java
@@ -20,4 +20,52 @@ public class Repo {
 
     @SerializedName("owner")
     User owner;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getStars() {
+        return stars;
+    }
+
+    public void setStars(int stars) {
+        this.stars = stars;
+    }
+
+    public int getForks() {
+        return forks;
+    }
+
+    public void setForks(int forks) {
+        this.forks = forks;
+    }
+
+    public User getOwner() {
+        return owner;
+    }
+
+    public void setOwner(User owner) {
+        this.owner = owner;
+    }
 }
