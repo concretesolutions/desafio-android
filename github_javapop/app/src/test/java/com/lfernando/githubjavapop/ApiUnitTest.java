7bd875b2fc0400ab5253ee2c79587bdcebcf2eea
Fernando Gomes <lfgsnando@gmail.com>
2019-06-18 11:52:07 -0300

    test api endpoints

---
+++ github_javapop/app/src/test/java/com/lfernando/githubjavapop/ApiUnitTest.java
@@ -0,0 +1,71 @@
+package com.lfernando.githubjavapop;
+
+import com.google.gson.Gson;
+import com.lfernando.githubjavapop.constants.Constants;
+import com.lfernando.githubjavapop.model.PullRequest;
+import com.lfernando.githubjavapop.model.Repo;
+import com.lfernando.githubjavapop.network.GithubApi;
+
+import org.junit.Test;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+public class ApiUnitTest {
+
+    @Test
+    public void listRepoApiTest() {
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(Constants.baseUrl)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+        GithubApi service = retrofit.create(GithubApi.class);
+        Call<List<Repo>> repos = service.listRepos("Java", "stars", 1);
+        repos.enqueue(new Callback<List<Repo>>() {
+            @Override
+            public void onResponse(Call<List<Repo>> call, Response<List<Repo>> response) {
+                List<Repo> repos = response.body();
+                assertNotNull(repos);
+                assertNotNull(repos.get(0).getName());
+                assertNotNull(repos.get(0).getOwner().getLogin());
+            }
+
+            @Override
+            public void onFailure(Call<List<Repo>> call, Throwable t) {
+                assertNotNull(t.getCause());
+            }
+        });
+    }
+
+    @Test
+    public void listPrs() {
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(Constants.baseUrl)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+        GithubApi service = retrofit.create(GithubApi.class);
+        Call<List<PullRequest>> prs = service.listPrs("CyC2018", "CS-Notes");
+        prs.enqueue(new Callback<List<PullRequest>>() {
+            @Override
+            public void onResponse(Call<List<PullRequest>> call, Response<List<PullRequest>> response) {
+                List<PullRequest> prs = response.body();
+                assertNotNull(prs);
+                assertNotNull(prs.get(0).getTitle());
+                assertNotNull(prs.get(0).getUser().getLogin());
+            }
+
+            @Override
+            public void onFailure(Call<List<PullRequest>> call, Throwable t) {
+                assertNotNull(t.getCause());
+            }
+        });
+    }
+}
