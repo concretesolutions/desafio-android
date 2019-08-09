3af8000a2a0682339edfa63be5f4f12ffa96784d
Sergio <sergio@Sergios-MacBook-Pro-756.local>
2019-08-09 15:29:34 -0300

    fixed pull request url

--- app/src/main/java/sergio/com/br/desafioandroid/app/Constants.kt
+++ app/src/main/java/sergio/com/br/desafioandroid/app/Constants.kt
@@ -4,7 +4,7 @@ object Constants {
 
     object END_POINTS {
         const val SEARCH_URL = "search/repositories?q=language:Java&sort=stars"
-        const val PULL_REQUEST_URL = "https://api.github.com/repos/{creator}/{repository}/pulls"
+        const val PULL_REQUEST_URL = "repos/{creator}/{repository}/pulls"
         const val PULL_REQUEST_HTML_URL = "https://github.com/{creator}/{repository}/pull/{number}"
     }
 }
