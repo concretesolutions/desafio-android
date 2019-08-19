d4d8fb8916ffff443c7606866d52bea582887d3d
João Domingos <j13domingos@gmail.com>
2019-08-19 02:26:11 -0300

    Preparando MainActivity para exibir a lista de repositórios

--- app/src/main/java/com/joaodomingos/desafio_android/ui/activities/MainActivity.kt
+++ app/src/main/java/com/joaodomingos/desafio_android/ui/activities/MainActivity.kt
@@ -2,12 +2,50 @@ package com.joaodomingos.desafio_android.ui.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.joaodomingos.desafio_android.R
+import com.joaodomingos.desafio_android.api.State
+import com.joaodomingos.desafio_android.ui.adapter.SearchListItemAdapter
+import com.joaodomingos.desafio_android.ui.view_models.SearchListItensViewModel
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var viewModel: SearchListItensViewModel
+    private lateinit var newsListAdapter: SearchListItemAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        viewModel = ViewModelProviders.of(this)
+            .get(SearchListItensViewModel::class.java)
+        initAdapter()
+        initState()
     }
+
+    private fun initAdapter() {
+        newsListAdapter = SearchListItemAdapter { viewModel.retry() }
+        recycler_view.layoutManager = LinearLayoutManager(this, RecyclerView.VERTICAL, false)
+        recycler_view.adapter = newsListAdapter
+        viewModel.newsList.observe(this, Observer {
+            newsListAdapter.submitList(it)
+        })
+    }
+
+    private fun initState() {
+        txt_error.setOnClickListener { viewModel.retry() }
+        viewModel.getState().observe(this, Observer { state ->
+            progress_bar.visibility = if (viewModel.listIsEmpty() && state == State.LOADING) View.VISIBLE else View.GONE
+            txt_error.visibility = if (viewModel.listIsEmpty() && state == State.ERROR) View.VISIBLE else View.GONE
+            if (!viewModel.listIsEmpty()) {
+                newsListAdapter.setState(state ?: State.DONE)
+            }
+        })
+    }
+
 }
