33ca0178bc78fe193f5815e91c214e66421d40a3
João Domingos <j13domingos@gmail.com>
2019-08-19 02:19:47 -0300

    Finalizando o ViewHolder

---
+++ app/src/main/java/com/joaodomingos/desafio_android/ui/holder/ListFooterViewHolder.kt
@@ -0,0 +1,27 @@
+package com.joaodomingos.desafio_android.ui.holder
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.View.VISIBLE
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.joaodomingos.desafio_android.R
+import com.joaodomingos.desafio_android.api.State
+import kotlinx.android.synthetic.main.item_list_footer.view.*
+
+class ListFooterViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+
+    fun bind(status: State?) {
+        itemView.progress_bar.visibility = if (status == State.LOADING) VISIBLE else View.INVISIBLE
+        itemView.txt_error.visibility = if (status == State.ERROR) VISIBLE else View.INVISIBLE
+    }
+
+    companion object {
+        fun create(retry: () -> Unit, parent: ViewGroup): ListFooterViewHolder {
+            val view = LayoutInflater.from(parent.context)
+                .inflate(R.layout.item_list_footer, parent, false)
+            view.txt_error.setOnClickListener { retry() }
+            return ListFooterViewHolder(view)
+        }
+    }
+}
---
+++ app/src/main/java/com/joaodomingos/desafio_android/ui/holder/SearchItemViewHolder.kt
@@ -0,0 +1,32 @@
+package com.joaodomingos.desafio_android.ui.holder
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.joaodomingos.desafio_android.R
+import com.joaodomingos.desafio_android.models.SearchItensModel
+import com.squareup.picasso.Picasso
+import kotlinx.android.synthetic.main.item_repo.view.*
+
+class SearchItemViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+
+    fun bind(searchItem: SearchItensModel?) {
+        if (searchItem != null) {
+            itemView.txt_name.text = searchItem.name
+            itemView.txt_fullname.text = searchItem.fullName
+            itemView.txt_forks .text = searchItem.forksCount.toString()
+            itemView.txt_stars.text = searchItem.starsCount.toString()
+            itemView.txt_username.text = searchItem.owner.userName
+            Picasso.get().load(searchItem.owner.avatarUrl).into(itemView.img_avatar)
+        }
+    }
+
+    companion object {
+        fun create(parent: ViewGroup): SearchItemViewHolder {
+            val view = LayoutInflater.from(parent.context)
+                .inflate(R.layout.item_repo, parent, false)
+            return SearchItemViewHolder(view)
+        }
+    }
+}
