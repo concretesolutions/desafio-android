f533e26d7c1b854f96cd9a5466390c7a29ef6bf0
João Domingos <j13domingos@gmail.com>
2019-08-18 23:15:22 -0300

    Adicionando a url para realizar o request

---
+++ app/src/main/java/com/joaodomingos/desafio_android/api/NetworkService.kt
@@ -0,0 +1,25 @@
+package com.joaodomingos.desafio_android.api
+
+import com.joaodomingos.desafio_android.models.SearchListItensModel
+import io.reactivex.Single
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface NetworkService {
+    @GET("search/repositories?q=language:Java&sort=stars")
+    fun getNews(@Query("page") page: Int): Single<SearchListItensModel>
+
+    companion object {
+        fun getService(): NetworkService {
+            val retrofit = Retrofit.Builder()
+                .baseUrl("https://api.github.com/")
+                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+            return retrofit.create(NetworkService::class.java)
+        }
+    }
+}
---
+++ app/src/main/java/com/joaodomingos/desafio_android/api/State.kt
@@ -0,0 +1,5 @@
+package com.joaodomingos.desafio_android.api
+
+enum class State {
+    DONE, LOADING, ERROR
+}
