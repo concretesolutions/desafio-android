3c8d311e844442daab0fa1a0c2d668bdebf38bec
Silvio Guedes <s.guedes.santana@gmail.com>
2019-09-14 18:02:47 -0300

    Injeção de dependência no fragmento de Detalhes.

--- app/src/main/java/com/silvioapps/githubkotlin/features/details/models/DetailsModel.kt
+++ app/src/main/java/com/silvioapps/githubkotlin/features/details/models/DetailsModel.kt
@@ -1,7 +1,9 @@
 package com.silvioapps.githubkotlin.features.details.models
 
 import java.io.Serializable
+import javax.inject.Singleton
 
+@Singleton
 data class DetailsModel(
 	val title: String? = null,
 	val body: String? = null,
--- app/src/main/java/com/silvioapps/githubkotlin/features/details/models/User.kt
+++ app/src/main/java/com/silvioapps/githubkotlin/features/details/models/User.kt
@@ -1,7 +1,9 @@
 package com.silvioapps.githubkotlin.features.details.models
 
 import java.io.Serializable
+import javax.inject.Singleton
 
+@Singleton
 data class User(
 	val login: String? = null,
 	val avatar_url: String? = null
