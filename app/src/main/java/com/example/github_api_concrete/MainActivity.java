1a752f17f0de8079494e3620d15fc62f576321b9
cecisousa <ceciliasousa21@gmail.com>
2020-01-07 20:14:45 -0200

    initial commit

---
+++ app/src/main/java/com/example/github_api_concrete/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.github_api_concrete;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
