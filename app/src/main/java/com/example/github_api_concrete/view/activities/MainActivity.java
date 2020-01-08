1d31e76e53e30ebbd602230b1bf984a2f9c4a546
cecisousa <ceciliasousa21@gmail.com>
2020-01-08 11:28:09 -0200

    implements mvvm and splash activity

--- app/src/main/java/com/example/github_api_concrete/view/activities/MainActivity.java
+++ app/src/main/java/com/example/github_api_concrete/view/activities/MainActivity.java
@@ -1,8 +1,10 @@
-package com.example.github_api_concrete;
+package com.example.github_api_concrete.view.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.example.github_api_concrete.R;
 
 public class MainActivity extends AppCompatActivity {
 
---
+++ app/src/main/java/com/example/github_api_concrete/view/activities/SplashActivity.java
@@ -0,0 +1,43 @@
+package com.example.github_api_concrete.view.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.example.github_api_concrete.R;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class SplashActivity extends AppCompatActivity {
+
+    private Timer timer = new Timer();
+    private ImageView splash;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        splash = findViewById(R.id.imageSplash);
+
+        splash.setOnClickListener(v -> {
+            jump();
+        });
+
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                jump();
+            }
+        },3000);
+    }
+
+    private void jump(){
+        timer.cancel();
+        startActivity(new Intent(SplashActivity.this, MainActivity.class));
+        finish();
+    }
+}
