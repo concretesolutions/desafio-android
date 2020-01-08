85a34dd9b29fe28c4198c1a9b362a42c296e9ff0
cecisousa <ceciliasousa21@gmail.com>
2020-01-08 12:43:55 -0200

    finishes layout of main activity/repos list

---
+++ app/src/main/java/com/example/github_api_concrete/view/activities/PullRequestsActivity.java
@@ -0,0 +1,16 @@
+package com.example.github_api_concrete.view.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.github_api_concrete.R;
+
+public class PullRequestsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pull_requests);
+    }
+}
