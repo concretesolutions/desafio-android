2419e30c2fd4bef5c5fcf70f610f704e42d8d891
GuilhermeP17 <guisil_ps@hotmail.com>
2020-01-08 22:37:45 -0300

    Implementando Endless Scroll

--- ProcessoConcrete/app/src/main/java/br/com/guilherme/concrete/view/MainActivity.java
+++ ProcessoConcrete/app/src/main/java/br/com/guilherme/concrete/view/MainActivity.java
@@ -1,5 +1,6 @@
 package br.com.guilherme.concrete.view;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.coordinatorlayout.widget.CoordinatorLayout;
 import androidx.recyclerview.widget.DividerItemDecoration;
@@ -12,6 +13,7 @@ import android.widget.ProgressBar;
 
 import com.google.android.material.snackbar.Snackbar;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import br.com.guilherme.concrete.R;
@@ -19,11 +21,16 @@ import br.com.guilherme.concrete.adapter.RepositorioAdapter;
 import br.com.guilherme.concrete.model.Repositorio;
 import br.com.guilherme.concrete.presenter.RepositorioPresenter;
 
-public class MainActivity extends AppCompatActivity implements RepositorioPresenter.View{
+public class MainActivity extends AppCompatActivity implements RepositorioPresenter.View {
     private CoordinatorLayout coordinatorLayout;
     private RecyclerView listRepo;
     private RepositorioPresenter presenter;
     private ProgressBar progressBar;
+    private int pageToLoad = 1;
+    private boolean isLoading = false;
+    private List<Repositorio> repositorios;
+    private RepositorioAdapter adapter;
+    private boolean isFirstPage = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,21 +41,45 @@ public class MainActivity extends AppCompatActivity implements RepositorioPresen
         progressBar = findViewById(R.id.loading_content);
         listRepo = findViewById(R.id.list_rpositorios);
 
+        repositorios = new ArrayList<>();
+
         presenter = new RepositorioPresenter(this);
-        presenter.getAllRepositorios(1);
+        presenter.getAllRepositorios(pageToLoad);
     }
 
     @Override
-    public void setRecyclerView(List<Repositorio> repositorios) {
+    public void setRecyclerView(final List<Repositorio> repositoriosFromApi) {
+        isLoading = false;
         progressBar.setVisibility(View.GONE);
         listRepo.setVisibility(View.VISIBLE);
 
-        RepositorioAdapter adapter = new RepositorioAdapter(repositorios, MainActivity.this, presenter);
-        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getApplicationContext());
+        int lastPosition = repositorios.size();
+        repositorios.addAll(repositoriosFromApi);
+        if (isFirstPage){
+            adapter = new RepositorioAdapter(repositorios, MainActivity.this, presenter);
+            final LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getApplicationContext());
 
-        listRepo.setLayoutManager(linearLayoutManager);
-        listRepo.setAdapter(adapter);
-        listRepo.addItemDecoration(new DividerItemDecoration(getApplicationContext(), DividerItemDecoration.VERTICAL));
+            listRepo.setLayoutManager(linearLayoutManager);
+            listRepo.setAdapter(adapter);
+            listRepo.addItemDecoration(new DividerItemDecoration(getApplicationContext(), DividerItemDecoration.VERTICAL));
+
+            repositorios.addAll(repositoriosFromApi);
+            isFirstPage = false;
+        }
+        adapter.notifyDataSetChanged();
+        listRepo.scrollToPosition(lastPosition);
+
+        listRepo.addOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrolled(@NonNull RecyclerView recyclerView, int dx, int dy) {
+                super.onScrolled(recyclerView, dx, dy);
+                LinearLayoutManager linearLayoutManager = (LinearLayoutManager) listRepo.getLayoutManager();
+                if (!isLoading && linearLayoutManager.findLastVisibleItemPosition() == repositorios.size() -1){
+                    isLoading = true;
+                    presenter.getAllRepositorios(++pageToLoad);
+                }
+            }
+        });
     }
 
     @Override
