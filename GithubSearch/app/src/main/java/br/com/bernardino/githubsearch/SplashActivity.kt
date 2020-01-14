32545b7520e322380fa66a66dce5d46894e55c5d
José Bernardino da Silva Júnior <jbernardino.jr@gmail.com>
2020-01-14 18:03:49 -0300

    [Github Search] Create a Splash activity and change the name oof MainActivity
    Signed-off-by: José Bernardino da Silva Júnior <jbernardino.jr@gmail.com>

---
+++ GithubSearch/app/src/main/java/br/com/bernardino/githubsearch/SplashActivity.kt
@@ -0,0 +1,42 @@
+package br.com.bernardino.githubsearch
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashActivity : AppCompatActivity() {
+    private var mDelayHandler: Handler? = null
+    private val SPLASH_DELAY: Long = 3000
+
+    internal val mRunnable: Runnable = Runnable {
+        if (!isFinishing) {
+
+            val intent = Intent(applicationContext, HomeActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        //Initialize the Handler
+        mDelayHandler = Handler()
+
+        //Navigate with delay
+        mDelayHandler!!.postDelayed(mRunnable, SPLASH_DELAY)
+
+    }
+
+    public override fun onDestroy() {
+
+        if (mDelayHandler != null) {
+            mDelayHandler!!.removeCallbacks(mRunnable)
+        }
+
+        super.onDestroy()
+    }
+
+}
