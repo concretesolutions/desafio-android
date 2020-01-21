deb163ccb5b78e23d8b16bcdbdabb2b8ccd8e1db
Raquel Santos <raquelmsantos.ol@gmail.com>
2020-01-20 22:12:23 -0300

    layouts

--- app/src/main/java/br/com/rmso/popularrepositories/PullRequestActivity.kt
+++ app/src/main/java/br/com/rmso/popularrepositories/PullRequestActivity.kt
@@ -3,10 +3,10 @@ package br.com.rmso.popularrepositories
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class PullRequestActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_pull_request)
     }
 }
