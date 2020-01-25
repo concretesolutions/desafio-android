0729aadedab7e61e01d12830a4b12dd88e39519a
Victor Amaral <vic_amaral_23@hotmail.com>
2020-01-24 23:41:04 -0300

    Primeiro commit. Adicionado drawer navigation

---
+++ app/src/main/java/br/com/victoramaral/githubdive/viewmodel/SendViewModel.java
@@ -0,0 +1,19 @@
+package br.com.victoramaral.githubdive.viewmodel;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class SendViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public SendViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is send fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
---
+++ app/src/main/java/br/com/victoramaral/githubdive/viewmodel/SlideshowViewModel.java
@@ -0,0 +1,19 @@
+package br.com.victoramaral.githubdive.viewmodel;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class SlideshowViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public SlideshowViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is slideshow fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
