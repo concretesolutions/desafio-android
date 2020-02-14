82273b32c79b2f26ee779736bde7139132d8be6c
Bernardo Júnior <bernardodemsj@gmail.com>
2020-02-14 00:43:59 -0300

    project initial files

--- app/src/main/java/com/concrete/desafio_android/ui/activity/HomeActivity.kt
+++ app/src/main/java/com/concrete/desafio_android/ui/activity/HomeActivity.kt
@@ -1,2 +1,12 @@
 package com.concrete.desafio_android.ui.activity
 
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class HomeActivity: AppCompatActivity(){
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+}
