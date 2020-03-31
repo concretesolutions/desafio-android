e0b6818de9cc1e6a425857c011436011c4ba1e3c
José Bernardino da Silva Júnior <jbernardino.jr@gmail.com>
2020-03-31 18:23:08 -0300

    [Github Search] Change splash activity to use kotlin coroutines
    Signed-off-by: José Bernardino da Silva Júnior <jbernardino.jr@gmail.com>

--- GithubSearch/app/src/main/java/br/com/bernardino/githubsearch/SplashActivity.kt
+++ GithubSearch/app/src/main/java/br/com/bernardino/githubsearch/SplashActivity.kt
@@ -2,41 +2,31 @@ package br.com.bernardino.githubsearch
 
 import android.content.Intent
 import android.os.Bundle
-import android.os.Handler
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.coroutines.*
 
 class SplashActivity : AppCompatActivity() {
-    private var mDelayHandler: Handler? = null
-    private val SPLASH_DELAY: Long = 3000
+    private val splashDelay: Long = 3000
 
-    internal val mRunnable: Runnable = Runnable {
-        if (!isFinishing) {
-
-            val intent = Intent(applicationContext, HomeActivity::class.java)
-            startActivity(intent)
-            finish()
-        }
-    }
+    private val splashActivityJob = SupervisorJob()
+    private val activityScope = CoroutineScope(splashActivityJob + Dispatchers.Main)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
-        //Initialize the Handler
-        mDelayHandler = Handler()
+        activityScope.launch {
+            delay(splashDelay)
 
-        //Navigate with delay
-        mDelayHandler!!.postDelayed(mRunnable, SPLASH_DELAY)
-
-    }
-
-    public override fun onDestroy() {
-
-        if (mDelayHandler != null) {
-            mDelayHandler!!.removeCallbacks(mRunnable)
+            var intent = Intent(this@SplashActivity, HomeActivity::class.java)
+            startActivity(intent)
+            finish()
         }
-
-        super.onDestroy()
     }
 
+    override fun onPause() {
+        splashActivityJob.cancel()
+        super.onPause()
+
+    }
 }
