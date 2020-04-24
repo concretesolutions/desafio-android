2d8c061a17b0ffc7e1ae68f649a330168e8cf54c
Bruno Rodrigues <bruno.rodrigues@segsat.com>
2020-04-24 11:25:12 -0300

    init

---
+++ app/src/main/java/com/concrete/challenge/githubjavapop/MainActivity.java
@@ -0,0 +1,21 @@
+package com.concrete.challenge.githubjavapop;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.concrete.challenge.githubjavapop.ui.main.MainFragment;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_activity);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow();
+        }
+    }
+}
---
+++ app/src/main/java/com/concrete/challenge/githubjavapop/ui/main/MainViewModel.java
@@ -0,0 +1,7 @@
+package com.concrete.challenge.githubjavapop.ui.main;
+
+import androidx.lifecycle.ViewModel;
+
+public class MainViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
