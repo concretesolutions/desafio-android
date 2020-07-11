0c048a2467312174a70e7553f9b5b3b65d0f7f6c
Mellina <p000mcerqueira@prservicos.com.br>
2020-07-10 22:37:46 -0300

    Adicionado layout do item repositorio

--- app/src/androidTest/java/com/bassul/mel/app/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/bassul/mel/app/ExampleInstrumentedTest.kt
@@ -1,24 +1,24 @@
 package com.bassul.mel.app
 
-  import android.support.test.InstrumentationRegistry
-  import android.support.test.runner.AndroidJUnit4
+import android.support.test.InstrumentationRegistry
+import android.support.test.runner.AndroidJUnit4
 
-  import org.junit.Test
-  import org.junit.runner.RunWith
+import org.junit.Test
+import org.junit.runner.RunWith
 
-  import org.junit.Assert.*
+import org.junit.Assert.*
 
-  /**
-   * Instrumented test, which will execute on an Android device.
-   *
-   * See [testing documentation](http://d.android.com/tools/testing).
-   */
-  @RunWith(AndroidJUnit4::class)
-  class ExampleInstrumentedTest {
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
     @Test
     fun useAppContext() {
-      // Context of the app under test.
-      val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-      assertEquals("com.bassul.mel.app", appContext.packageName)
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+        assertEquals("com.bassul.mel.app", appContext.packageName)
     }
-  }
+}
--- app/src/main/java/com/bassul/mel/app/MainActivity.kt
+++ app/src/main/java/com/bassul/mel/app/MainActivity.kt
@@ -1,12 +1,15 @@
 package com.bassul.mel.app
 
-import android.support.v7.app.AppCompatActivity
+
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
     }
 }
