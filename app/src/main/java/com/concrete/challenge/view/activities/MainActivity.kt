4c58a950ba80d307c569d94c40e3c2f4b544c94e
Francisca Hernández <panchy.h@live.cl>
2021-09-22 17:59:25 -0300

    Initial commit

---
+++ app/src/main/java/com/concrete/challenge/view/activities/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.concrete.challenge.view.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.concrete.challenge.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
