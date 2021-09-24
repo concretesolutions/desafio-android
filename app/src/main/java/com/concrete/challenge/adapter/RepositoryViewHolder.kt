f8340c4d9e480f7d0af771ab97f0b951efb9fda7
Francisca Hernández <panchy.h@live.cl>
2021-09-24 15:51:40 -0300

    Displayed repositories list with dummy data

---
+++ app/src/main/java/com/concrete/challenge/adapter/RepositoryViewHolder.kt
@@ -0,0 +1,23 @@
+package com.concrete.challenge.adapter
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+import com.concrete.challenge.data.RepositoryEntity
+import com.concrete.challenge.databinding.ItemRepositoryBinding
+
+class RepositoryViewHolder(
+    view: View,
+) : RecyclerView.ViewHolder(view) {
+
+    private val binding = ItemRepositoryBinding.bind(view)
+
+    fun bind(item: RepositoryEntity) {
+        binding.txtUsername.text = item.username
+        binding.txtUserName.text = item.userName
+        binding.txtRepositoryName.text = item.repositoryName
+        binding.txtRepositoryDescription.text = item.repositoryDescription
+        binding.txtForksAmount.text = item.forksAmount.toString()
+        binding.txtStarGazersAmount.text = item.starsAmount.toString()
+    }
+
+}
