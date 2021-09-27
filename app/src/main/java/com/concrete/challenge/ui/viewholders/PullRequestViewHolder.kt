3f36ea0b5e74c679c7a2d4236cd1a893a24632f4
Francisca Hernández <panchy.h@live.cl>
2021-09-27 14:13:26 -0300

    Pull request ui fixed

--- app/src/main/java/com/concrete/challenge/ui/viewholders/PullRequestViewHolder.kt
+++ app/src/main/java/com/concrete/challenge/ui/viewholders/PullRequestViewHolder.kt
@@ -5,6 +5,7 @@ import androidx.recyclerview.widget.RecyclerView
 import com.concrete.challenge.data.PullRequestEntity
 import com.concrete.challenge.databinding.ItemPullRequestBinding
 import com.concrete.challenge.ui.adapters.PullRequestAdapter
+import com.concrete.challenge.ui.fragments.PullRequestFragment
 
 class PullRequestViewHolder(
     private val view: View,
@@ -14,8 +15,8 @@ class PullRequestViewHolder(
     private val binding = ItemPullRequestBinding.bind(view)
 
     fun bind(item: PullRequestEntity) {
-        binding.txtOpenPullRequestAmount.text = item.openPullRequestAmount.toString()
-        binding.txtClosedPullRequestAmount.text = item.closedPullRequestAmount.toString()
+        //binding.txtOpenPullRequestAmount.text = item.openPullRequestAmount.toString()
+        //binding.txtClosedPullRequestAmount.text = item.closedPullRequestAmount.toString()
         binding.txtPullRequestTitle.text = item.pullRequestTitle
         binding.txtPullRequestBody.text = item.pullRequestBody
         binding.txtUsername.text = item.username
