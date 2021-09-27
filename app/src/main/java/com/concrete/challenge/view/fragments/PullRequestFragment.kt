7f1867baa55dc101c1f48c7b0eba7fe84e10a354
Francisca Hernández <panchy.h@live.cl>
2021-09-27 11:41:36 -0300

    Added ui for pull requests list

---
+++ app/src/main/java/com/concrete/challenge/view/fragments/PullRequestFragment.kt
@@ -0,0 +1,23 @@
+package com.concrete.challenge.view.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.concrete.challenge.R
+import com.concrete.challenge.databinding.FragmentPullRequestBinding
+
+class PullRequestFragment : Fragment() {
+
+    private lateinit var binding: FragmentPullRequestBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentPullRequestBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+}
